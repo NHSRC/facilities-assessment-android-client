6668d9763421805c622a4ea652bff024ac696a01
mihirk <mihir.khatwani@gmail.com>
2017-01-23 11:21:20 +0530

    Adding listing items as a separate component for color customization

---
+++ src/js/views/common/ListingItem.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {Dimensions, View, Text, TouchableWithoutFeedback, StyleSheet} from 'react-native';
+import AbstractComponent from "../common/AbstractComponent";
+import PrimaryColors from "../styles/PrimaryColors";
+import Typography from "../styles/Typography";
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
+class ListingItem extends AbstractComponent {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    static styles = StyleSheet.create({
+        listingContainer: {
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+            justifyContent: 'flex-start',
+            alignItems: 'stretch',
+            marginTop: deviceHeight * 0.02,
+        },
+        listingButton: {
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            margin: 0,
+            marginTop: deviceHeight * .01667,
+            height: deviceHeight * 0.06,
+        },
+        listingButtonText: {
+            backgroundColor: PrimaryColors.light_black,
+            width: deviceWidth * 0.72,
+            paddingLeft: deviceHeight * .02,
+            margin: 0,
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
+        },
+        listingButtonLabel: {
+            width: deviceWidth * 0.2,
+            margin: 0,
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+    });
+
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={this.props.onPress(item)} key={idx}>
+                <View style={Listing.styles.listingButton}>
+                    <View style={Listing.styles.listingButtonText}>
+                        <Text style={[Typography.paperFontSubhead, {color: PrimaryColors.subheader_black}]}>
+                            {item.name}
+                        </Text>
+                    </View>
+                    <View style={[Listing.styles.listingButtonLabel, {backgroundColor: this.props.labelColor}]}>
+                        <Text style={[Typography.paperFontBody1, {color: "#FFF"}]}>
+                            {item.reference}
+                        </Text>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>);
+    }
+}
+
+export default ListingItem;
