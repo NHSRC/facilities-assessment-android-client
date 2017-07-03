95f471e8cec1a3fe34400df1c47469b170e565e6
mihirk <mihir.khatwani@gmail.com>
2017-07-04 03:39:04 +0530

    Adding swipe support to the assessment interface

--- src/js/views/assessment/Assessment.js
+++ src/js/views/assessment/Assessment.js
@@ -13,7 +13,8 @@ import QuestionAnswer from './QuestionAnswer';
 import Pagination from './Pagination';
 import _ from 'lodash';
 import Dashboard from '../dashboard/Dashboard';
-import SubmitButton from "../common/SubmitButton";
+import GestureRecognizer from 'react-native-swipe-gestures';
+
 
 const deviceWidth = Dimensions.get('window').width;
 const deviceHeight = Dimensions.get('window').height;
@@ -42,8 +43,29 @@ class Assessment extends AbstractComponent {
         this.unsubscribe();
     }
 
+    getCurrentIndex() {
+        return _.findIndex(this.state.checkpoints,
+            (checkpoint) => this.state.currentCheckpoint.uuid === checkpoint.uuid);
+    }
+
+    onNext() {
+        let nextIndex = this.getCurrentIndex() + 1;
+        const nextCheckpoint = this.state.checkpoints[nextIndex >= this.state.checkpoints.length ? nextIndex - 1 : nextIndex];
+        this.dispatchAction(Actions.CHANGE_PAGE, {currentCheckpoint: nextCheckpoint});
+    }
+
+    onPrev() {
+        let prevIndex = this.getCurrentIndex() - 1;
+        const prevCheckpoint = this.state.checkpoints[prevIndex < 0 ? 0 : prevIndex];
+        this.dispatchAction(Actions.CHANGE_PAGE, {currentCheckpoint: prevCheckpoint});
+    }
+
 
     render() {
+        const config = {
+            velocityThreshold: 0.1,
+            directionalOffsetThreshold: 80
+        };
         return (
             <Container theme={FlatUITheme}>
                 <Header style={Dashboard.styles.header}>
@@ -56,14 +78,20 @@ class Assessment extends AbstractComponent {
                     </Title>
                 </Header>
                 <Content>
-                    <View style={{margin: deviceWidth * 0.04,}}>
-                        <QuestionAnswer goBack={() => TypedTransition.from(this).goBack()}
-                                        checkpoints={this.state.checkpoints}
-                                        currentCheckpoint={this.state.currentCheckpoint}
-                            {...this.props}/>
-                        <Pagination currentCheckpoint={this.state.currentCheckpoint}
-                                    checkpoints={this.state.checkpoints}/>
-                    </View>
+                    <GestureRecognizer
+                        onSwipeLeft={this.onNext.bind(this)}
+                        onSwipeRight={this.onPrev.bind(this)}
+                        config={config}
+                        style={{flex: 1,}}>
+                        <View style={{margin: deviceWidth * 0.04,}}>
+                            <QuestionAnswer goBack={() => TypedTransition.from(this).goBack()}
+                                            checkpoints={this.state.checkpoints}
+                                            currentCheckpoint={this.state.currentCheckpoint}
+                                            {...this.props}/>
+                            <Pagination currentCheckpoint={this.state.currentCheckpoint}
+                                        checkpoints={this.state.checkpoints}/>
+                        </View>
+                    </GestureRecognizer>
                 </Content>
             </Container>
         );
