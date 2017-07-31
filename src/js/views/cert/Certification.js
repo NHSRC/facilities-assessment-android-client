650e74a9a8b3b9d7895b9f3436ffb9a2a21c582c
mihirk <mihir.khatwani@gmail.com>
2017-07-31 16:17:11 +0530

    Styling the certification screen

--- src/js/views/cert/Certification.js
+++ src/js/views/cert/Certification.js
@@ -6,8 +6,12 @@ import Actions from '../../action';
 import TypedTransition from "../../framework/routing/TypedTransition";
 import Path from "../../framework/routing/Path";
 import Typography from "../styles/Typography";
+import PrimaryColors from "../styles/PrimaryColors";
+import {Button, Container, Content, Header, Icon, Title} from "native-base";
+import FlatUITheme from '../themes/flatUI';
 
 const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 
 @Path("/certification")
 class Certification extends AbstractComponent {
@@ -18,7 +22,37 @@ class Certification extends AbstractComponent {
         this.unsubscribe = store.subscribeTo('certification', this.handleChange.bind(this));
     }
 
-    static styles = StyleSheet.create({});
+    static styles = StyleSheet.create({
+        header: {
+            shadowOffset: {width: 0, height: 0},
+            elevation: 0,
+            backgroundColor: '#212121',
+        },
+        container: {
+            padding: 10,
+            backgroundColor: PrimaryColors.blue,
+            height: deviceHeight * .2,
+        },
+        innerContainer: {
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        criteriaList: {
+            justifyContent: 'center',
+            alignItems: 'stretch',
+        },
+        criteriaListItem: {
+            flexDirection: 'row',
+            flexWrap: 'nowrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: 10,
+            borderWidth: 1,
+            borderColor: PrimaryColors.medium_white
+        }
+    });
 
     handleChange() {
         const newState = this.context.getStore().getState().certification;
@@ -35,16 +69,61 @@ class Certification extends AbstractComponent {
 
     render() {
         let Criteria = this.state.criteria.map((criteria, idx) =>
-            <Text key={idx} style={[Typography.paperFontHeadline, {color: 'white'}]}>
-                {criteria.name} - {String(criteria.certified)}
-            </Text>);
-        return (
-            <View>
-                <Text style={[Typography.paperFontHeadline, {color: 'white'}]}>
-                    Certification - {String(this.state.certified)}
+            <View key={idx} style={[Certification.styles.criteriaListItem,
+                {backgroundColor: criteria.certified ? "#47A64A" : "#FF8880"}]}>
+                <Text style={[Typography.paperFontHeadline, {color: "white"}]}>
+                    {criteria.name}
                 </Text>
-                {Criteria}
-            </View>
+                <Icon name={criteria.certified ? "thumb-up" : "thumb-down"}
+                      style={{color: "white",}}/>
+            </View>);
+        const iconName = this.state.certified ? "stars" : "block";
+        const certificationText = this.state.certified ? "Certified" : "Not Certified";
+        return (
+            <Container theme={FlatUITheme}>
+                <Header style={Certification.styles.header}>
+                    <Button
+                        onPress={() => TypedTransition.from(this).goBack()}
+                        transparent>
+                        <Icon style={{marginTop: 10, color: 'white'}} name="arrow-back"/>
+                    </Button>
+                    <Title style={[Typography.paperFontHeadline, {
+                        fontWeight: 'bold',
+                        color: 'white'
+                    }]}>
+                        Certification Criteria
+                    </Title>
+                </Header>
+                <Content ref="reports">
+                    <View style={[Certification.styles.container,
+                        {backgroundColor: this.state.certified ? "#3C8C3F" : "#8C2518"}]}>
+                        <View style={{marginBottom: 10}}>
+                            <Text style={[Typography.paperFontTitle, {color: "white", alignSelf: 'center'}]}>
+                                {this.props.params.facility.name}
+                            </Text>
+                        </View>
+                        <View style={Certification.styles.innerContainer}>
+                            <Icon name={iconName} style={{fontSize: 100, color: "white"}} size={100}/>
+                            <Text style={[Typography.paperFontHeadline, {color: "white"}]}>
+                                {certificationText}
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={{
+                        backgroundColor: PrimaryColors.dark_white,
+                        padding: 5,
+                        paddingTop: 10,
+                        paddingBottom: 10
+                    }}>
+                        <Text style={[Typography.paperFontHeadline, {color: 'white'}]}>
+                            Criterion List
+                        </Text>
+                    </View>
+                    <View style={Certification.styles.criteriaList}>
+                        {Criteria}
+                    </View>
+                </Content>
+            </Container>
         );
     }
 }
