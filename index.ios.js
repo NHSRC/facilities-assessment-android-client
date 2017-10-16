bcb8daf1332a2f66cf966910245011ebb925e50c
Vivek Singh <petmongrels@gmail.com>
2017-10-16 22:13:43 +0530

    linked to react native view shot

--- index.ios.js
+++ index.ios.js
@@ -3,45 +3,32 @@ import {
     AppRegistry,
     StyleSheet,
     Text,
-    View
+    View,
+    StatusBar
 } from 'react-native';
+import App from './src/js/App';
 
 export default class FacilitiesAssessment extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
+            <View style={FacilitiesAssessment.styles.container}>
+                <StatusBar
+                    backgroundColor={"#212121"}
+                    barStyle="light-content"
+                />
+                <App />
             </View>
         );
     }
+
+    static styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'stretch',
+            backgroundColor: '#303030'
+        }
+    });
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
-
+// console.disableYellowBox = true;
 AppRegistry.registerComponent('FacilitiesAssessment', () => FacilitiesAssessment);
