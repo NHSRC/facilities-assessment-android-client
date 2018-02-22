b5d738afccb98fde5013d75e3913c44c7d254f2e
Vivek Singh <petmongrels@gmail.com>
2018-02-22 11:06:59 +0530

    Submit facility name along with facility uuid

--- src/js/mapper/facilityAssessmentMapper.js
+++ src/js/mapper/facilityAssessmentMapper.js
@@ -1,10 +1,11 @@
 import {formatDate} from '../utility/DateUtils';
 
 export default facilityAssessmentMapper =
-    ({uuid, facility: {uuid: facilityUUID}, assessmentTool: {uuid: assessmentToolUUID}, assessmentType: {uuid: assessmentTypeUUID}, startDate, endDate, seriesName, deviceId}) =>
+    ({uuid, facility: {uuid: facilityUUID, name: name}, assessmentTool: {uuid: assessmentToolUUID}, assessmentType: {uuid: assessmentTypeUUID}, startDate, endDate, seriesName, deviceId}) =>
         Object.assign({
             uuid: uuid,
             facility: facilityUUID,
+            facilityName: name,
             assessmentTool: assessmentToolUUID,
             startDate: formatDate(startDate),
             endDate: formatDate(endDate),
