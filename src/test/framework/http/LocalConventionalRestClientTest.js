a09c5ca8ec366967779398dac2e9ea01247ec135
Vivek Singh <petmongrels@gmail.com>
2018-02-25 16:14:05 +0530

    LocalConventionalRestClient responds based on the url timestamp

--- src/test/framework/http/LocalConventionalRestClientTest.js
+++ src/test/framework/http/LocalConventionalRestClientTest.js
@@ -5,6 +5,8 @@ import SettingsService from "../../../js/service/SettingsService";
 import EntitiesMetaData from "../../../js/models/entityMetaData/EntitiesMetaData";
 
 describe('Local Conventional Rest Client', () => {
+    const defaultDateTime = '2017-01-01T00:00:00.001Z';
+
     const create = function (typeName, pagesContent) {
         let pages = [];
         pagesContent.forEach((pageContent) => {
@@ -16,19 +18,23 @@ describe('Local Conventional Rest Client', () => {
         return pages;
     };
 
-    const obj = function (str, date) {
-        return {name: str, lastModifiedDate: date};
+    const obj = function (str, dateTime = defaultDateTime) {
+        return {name: str, lastModifiedDate: dateTime};
     };
 
     const getObject = function (typeName, response, index) {
         return response['_embedded'][typeName][index];
     };
 
+    const getURL = function (entityType, dateTime = defaultDateTime) {
+        return `http://localhost/api/${entityType}/search/lastModified?lastModifiedDate=${dateTime}&size=200&page=0`;
+    };
+
     let localConventionalRestClient;
 
     beforeEach(() => {
-        let fileDataMap = new Map([['common', create('facilityType', [[obj('CHC', "2017-11-05T11:58:47.671+0000"), obj('DH', "2017-11-05T11:58:47.671+0000")], [obj('PHC', "2017-11-06T11:58:47.671+0000"), obj('CHC', "2017-11-06T11:58:47.671+0000")]])],
-            ['Bihar', create('district', [[obj('Aarah', "2017-11-06T11:58:47.671+0000"), obj('Buxar', "2017-11-06T11:58:47.671+0000")], [obj('Patna', "2017-11-06T11:58:47.671+0000"), obj('Danapur', "2017-11-06T11:58:47.671+0000")]])]]);
+        let fileDataMap = new Map([['common', create('facilityType', [[obj('CHC'), obj('DH'), obj('MC', '2017-08-01T00:00:00.001Z')], [obj('PHC'), obj('CHC')]])],
+            ['Bihar', create('district', [[obj('Aarah'), obj('Buxar')], [obj('Patna'), obj('Danapur')]])]]);
         let testContext = new TestContext();
         localConventionalRestClient = new LocalConventionalRestClient(testContext.getService(SettingsService), undefined, fileDataMap);
     });
@@ -37,14 +43,16 @@ describe('Local Conventional Rest Client', () => {
         let cbFile;
         let referenceEntityTypes = EntitiesMetaData.referenceEntityTypes;
         let facilityTypeMetadata = referenceEntityTypes[referenceEntityTypes.length - 1];
-        localConventionalRestClient.getData('ft', facilityTypeMetadata, {}, (file) => {
+        localConventionalRestClient.getData(getURL('facilityType'), facilityTypeMetadata, {}, (file) => {
             cbFile = file
         });
         expect(getObject('facilityType', cbFile, 0).name).to.be.equal('CHC');
-        localConventionalRestClient.getData('ft', facilityTypeMetadata, {}, (file) => {
+        expect(getObject('facilityType', cbFile, 1).name).to.be.equal('DH');
+        localConventionalRestClient.getData(getURL('facilityType'), facilityTypeMetadata, {}, (file) => {
             cbFile = file
         });
         expect(getObject('facilityType', cbFile, 0).name).to.be.equal('PHC');
+        expect(getObject('facilityType', cbFile, 1).name).to.be.equal('CHC');
     });
 
     it("get state specific data", () => {
@@ -52,16 +60,23 @@ describe('Local Conventional Rest Client', () => {
         let referenceEntityTypes = EntitiesMetaData.referenceEntityTypes;
         let districtEntityMetadata = referenceEntityTypes[referenceEntityTypes.length - 3];
         let optionalParams = {name: 'Bihar'};
-        localConventionalRestClient.getData('foo', districtEntityMetadata, optionalParams, (file) => {
+        localConventionalRestClient.getData(getURL('district'), districtEntityMetadata, optionalParams, (file) => {
             cbFile = file
         });
         expect(getObject('district', cbFile, 0).name).to.be.equal('Aarah');
-        localConventionalRestClient.getData('foo', districtEntityMetadata, optionalParams, (file) => {
+        localConventionalRestClient.getData(getURL('district'), districtEntityMetadata, optionalParams, (file) => {
             cbFile = file
         });
         expect(getObject('district', cbFile, 0).name).to.be.equal('Patna');
     });
 
     it('should return only one object ignoring all other objects', () => {
+        let cbFile;
+        let referenceEntityTypes = EntitiesMetaData.referenceEntityTypes;
+        let facilityTypeMetadata = referenceEntityTypes[referenceEntityTypes.length - 1];
+        localConventionalRestClient.getData(getURL('facilityType', '2017-02-01T00:00:00.001Z'), facilityTypeMetadata, {}, (file) => {
+            cbFile = file
+        });
+        expect(getObject('facilityType', cbFile, 0).name).to.be.equal('MC');
     });
 });
