f5ff548d51d42ae6bad254d6d0b151a3d7bcd448
Vivek Singh <petmongrels@gmail.com>
2019-06-28 15:38:00 +0530

    removed unnecessary padding

--- src/js/views/dashboard/start/AssessmentTools.js
+++ src/js/views/dashboard/start/AssessmentTools.js
@@ -20,9 +20,7 @@ class AssessmentTools extends AbstractComponent {
             color: "white",
         },
         inactiveButton: {
-            width: deviceWidth * 0.44,
-            paddingTop: 16,
-            paddingBottom: 16,
+            width: deviceWidth * 0.46,
             marginTop: deviceheight * 0.02667,
             flexDirection: "row",
             justifyContent: 'flex-start',
@@ -32,8 +30,6 @@ class AssessmentTools extends AbstractComponent {
         },
         activeButton: {
             width: deviceWidth * 0.44,
-            paddingTop: 16,
-            paddingBottom: 16,
             marginTop: deviceheight * 0.02667,
             backgroundColor: PrimaryColors.blue,
             flexDirection: "row",
@@ -65,6 +61,7 @@ class AssessmentTools extends AbstractComponent {
                         key={idx}
                         style={isSelected ? AssessmentTools.styles.activeButton : AssessmentTools.styles.inactiveButton}
                         iconLeft={true}
+                        primary
                         onPress={this.onSelect(assessmentTool)}>
                         <Icon style={{color: isSelected ? "white" : PrimaryColors.subheader_black}}
                               name='list-box'/><Text>{assessmentTool.name}</Text>
