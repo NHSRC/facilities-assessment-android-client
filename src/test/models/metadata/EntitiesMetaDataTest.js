c5f8ae4d777c173234e6f0bf28bdb6c5d54f3b68
Vivek Singh <petmongrels@gmail.com>
2020-02-21 16:34:30 +0530

    fixed unit test

--- src/test/models/metadata/EntitiesMetaDataTest.js
+++ src/test/models/metadata/EntitiesMetaDataTest.js
@@ -5,10 +5,10 @@ import AssessmentTool from "../../../js/models/AssessmentTool";
 describe('EntitiesMetaDataTest', () => {
     it('check counts and order', () => {
         expect(EntitiesMetaData.txEntityTypes.length).is.equal(4);
-        expect(EntitiesMetaData.stateSpecificReferenceEntityTypes.length).is.equal(4);
+        expect(EntitiesMetaData.stateSpecificReferenceEntityTypes.length).is.equal(6);
 
-        expect(EntitiesMetaData.allEntityTypes.length).is.equal(19);
+        expect(EntitiesMetaData.allEntityTypes.length).is.equal(21);
         expect(EntitiesMetaData.allEntityTypes[0].entityClass).is.equal(FacilityAssessmentProgress);
-        expect(EntitiesMetaData.allEntityTypes[16].entityClass).is.equal(AssessmentTool);
+        expect(EntitiesMetaData.allEntityTypes[18].entityClass).is.equal(AssessmentTool);
     });
 });
