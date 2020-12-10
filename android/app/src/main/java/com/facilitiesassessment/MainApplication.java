e5839568c221748c2081890b839d7e3e7387dd9c
Vivek Singh <petmongrels@gmail.com>
2020-12-10 17:48:54 +0530

    KeepAwake when progress bar is active

--- android/app/src/main/java/com/facilitiesassessment/MainApplication.java
+++ android/app/src/main/java/com/facilitiesassessment/MainApplication.java
@@ -3,6 +3,7 @@ package com.facilitiesassessment;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.corbt.keepawake.KCKeepAwakePackage;
 import com.horcrux.svg.SvgPackage;
 import com.avishayil.rnrestart.ReactNativeRestartPackage;
 import com.bugsnag.BugsnagReactNative;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                 new MainReactPackage(),
+            new KCKeepAwakePackage(),
             new SvgPackage(),
             new ReactNativeRestartPackage(),
                 BugsnagReactNative.getPackage(),
--- src/js/views/progressBar/PlatformIndependentProgressBar.js
+++ src/js/views/progressBar/PlatformIndependentProgressBar.js
@@ -1,11 +1,16 @@
 import React from "react";
 import {Platform, ProgressBarAndroid, ProgressViewIOS} from "react-native";
+import KeepAwake from 'react-native-keep-awake';
 
 export default class PlatformIndependentProgressBar {
     static display(progress, style = {}, color = "white") {
         if (Platform.OS === 'android')
-            return <ProgressBarAndroid styleAttr="Horizontal" progress={progress} indeterminate={false} color="white" style={style}/>;
+            return <>
+                <KeepAwake/>
+                <ProgressBarAndroid styleAttr="Horizontal" progress={progress} indeterminate={false} color="white" style={style}/>
+            </>;
         else
-            return <ProgressViewIOS progress={progress} progressViewStyle="bar" trackTintColor="white" style={style}/>;
+            return <><KeepAwake />
+                <ProgressViewIOS progress={progress} progressViewStyle="bar" trackTintColor="white" style={style}/></>;
     }
 }
