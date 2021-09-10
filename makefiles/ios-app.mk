cbf2700912baebd3668330535ec5b1b1f347b659
Vivek Singh <petmongrels@gmail.com>
2021-09-10 12:38:35 +0530

    run a specific version of simulator

--- makefiles/ios-app.mk
+++ makefiles/ios-app.mk
@@ -1,26 +1,3 @@
-#--simulator="iPhone 5s"
-#--simulator="iPhone 6"
-#--simulator="iPhone 6 Plus"
-#--simulator="iPhone 6s"
-#--simulator="iPhone 6s Plus"
-#--simulator="iPhone 7"
-#--simulator="iPhone 7 Plus"
-#--simulator="iPhone 8"
-#--simulator="iPhone 8 Plus"
-#--simulator="iPhone SE"
-#--simulator="iPhone X"
-#--simulator="iPhone XR"
-#--simulator="iPhone Xs"
-#--simulator="iPhone Xs Max"
-#--simulator="iPad Air"
-#--simulator="iPad Air 2"
-#--simulator="iPad"
-#--simulator="iPad Pro"
-#--simulator="iPad Pro"
-#--simulator="iPad Pro"
-#--simulator="iPad Pro"
-#--simulator="iPad"
-
 define _run_ios_dev
 	$(call _setup_hosts,$1)
 	$(call _create_config,dev)
@@ -29,7 +6,7 @@ endef
 
 define _run_ios
 	$(call _create_config,$1)
-	react-native run-ios --simulator="iPhone 6 Plus"
+	react-native run-ios --simulator=$2
 endef
 
 define _switch_ios_to_mode
@@ -39,8 +16,11 @@ endef
 open-simulator:
 	open -a simulator
 
+run-app-ios-nhsrc-default:
+	$(call _run_ios,nhsrc,"iPhone 8 Plus")
+
 run-app-ios-nhsrc:
-	$(call _run_ios,nhsrc)
+	$(call _run_ios,nhsrc,"$(iphone)")
 
 run-app-ios-nhsrc-release:
 	$(call _run_ios,nhsrc)
@@ -50,6 +30,7 @@ run-app-ios-nhsrc-dev:
 
 prepare-ipa-nhsrc:
 	$(call _create_config,nhsrc)
+	react-native bundle --entry-file index.ios.js --platform ios --dev false --bundle-output ios/main.jsbundle --assets-dest ios
 
 release-ios-nhsrc:
 	$(call _create_config,nhsrc)
